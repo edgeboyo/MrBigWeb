e3e16a91fedeff47c016b2f0c6fb8fc11014345c
edgeboyo <mkolybko@gmail.com>
2022-01-31 10:22:39 +0000

    Simple API pull

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,14 @@
 import db from "./app.js";
-import { doc, collection } from "firebase/firestore";
+import { doc, collection, getDoc } from "firebase/firestore";
 
 const pathsRef = collection(db, "paths");
 
 export async function readPath(pathId) {
-  const path = await doc(pathsRef);
+  const path = await getDoc(doc(pathsRef, pathId));
+
+  if (!path.exists()) {
+    return null;
+  }
+
+  return path.data();
 }
